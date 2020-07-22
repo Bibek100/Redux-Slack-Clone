0b6bfd5ad5ecafab2e3c0fe71f3d1d2d5d18b97e
Bibek100 <vivek.dahal10@gmail.com>
2020-07-22 15:23:10 -0500

    First channel added

--- src/components/SidePanel/Channels.js
+++ src/components/SidePanel/Channels.js
@@ -1,11 +1,53 @@
 import React from "react";
 import { Menu, Icon, Modal, Form, Input, Button } from "semantic-ui-react";
+import firebase from "../../firebase";
+import UserPanel from "./UserPanel";
 
 class Channels extends React.Component {
   state = {
     channels: [],
+    channelName: "",
+    channelDetails: "",
+    channelsRef: firebase.database().ref("channels"),
     modal: false,
+    user: this.props.currentUser,
   };
+
+  addchannel = () => {
+    const { channelsRef, channelDetails, channelName, user } = this.state;
+
+    const key = channelsRef.push().key;
+    const newChannel = {
+      id: key,
+      name: channelName,
+      details: channelDetails,
+      createdBy: {
+        name: user.displayName,
+        avatar: user.photoURL,
+      },
+    };
+
+    channelsRef
+      .child(key)
+      .update(newChannel)
+      .then(() => {
+        this.setState({ channelName: "", channelDetails: "" });
+        this.closeModal();
+        console.log("channel added");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  handleSubmit = (event) => {
+    event.preventDefault();
+    if (this.isFormValid(this.state)) {
+      this.addchannel();
+    }
+  };
+
+  isFormValid = ({ channelName, channelDetails }) =>
+    channelName && channelDetails;
 
   closeModal = () => {
     this.setState({ modal: false });
@@ -45,13 +87,13 @@ class Channels extends React.Component {
               <Input
                 fluid
                 label="About the channel"
-                name="channelDetails;"
+                name="channelDetails"
                 onChange={this.handleChange}
               />
             </Form>
           </Modal.Content>
           <Modal.Actions>
-            <Button color="green" inverted>
+            <Button color="green" inverted onClick={this.handleSubmit}>
               <Icon name="checkmark" />
               Add
             </Button>
