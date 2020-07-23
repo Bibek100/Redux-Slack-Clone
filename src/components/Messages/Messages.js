ddb3a49374c85ab96c019cb0d4688223d344e5fb
Bibek100 <vivek.dahal10@gmail.com>
2020-07-22 21:01:35 -0500

    First message s added to database

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,18 +1,30 @@
 import React from "react";
+import firebase from "../../firebase";
 
 import { Segment, Comment } from "semantic-ui-react";
 import MessagesHeader from "./MessagesHeader";
 import MessageForm from "./MesssageForm";
 
 class Messages extends React.Component {
+  state = {
+    messagesRef: firebase.database.ref("messages"),
+    channel: this.props.currentChannel,
+    user: this.props.currentUser,
+  };
+
   render() {
+    const { messagesRef, channel, user } = this.state;
     return (
       <React.Fragment>
         <MessagesHeader />
         <Segment>
           <Comment.Group className="messages"></Comment.Group>
         </Segment>
-        <MessageForm />
+        <MessageForm
+          messagesRef={messagesRef}
+          currentChannel={channel}
+          currentUser={user}
+        />
       </React.Fragment>
     );
   }
