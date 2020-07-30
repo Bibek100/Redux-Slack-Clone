62a673623da61666b220794e5b8162b97f1ed742
Bibek100 <vivek.dahal10@gmail.com>
2020-07-29 22:27:30 -0500

    Used reducx using connect in starred componnet to get starred channel

--- src/components/SidePanel/Starred.js
+++ src/components/SidePanel/Starred.js
@@ -1,8 +1,40 @@
 import React from "react";
+import { connect } from "react-redux";
+import { setCurrentChannel, setPrivateChannel } from "../../actions";
 import { Menu, Icon } from "semantic-ui-react";
 
 class Starred extends React.Component {
+  state = {
+    activechannel: "",
+    starredChannels: [],
+  };
+  setActiveChannel = (channel) => {
+    this.setState({ activechannel: channel.id });
+  };
+  changeChannel = (channel) => {
+    this.setActiveChannel(channel);
+
+    this.props.setCurrentChannel(channel);
+    this.props.setPrivateChannel(false);
+  };
+  displayChannels = (starredchannels) => {
+    return (
+      starredchannels.length > 0 &&
+      starredchannels.map((channel) => (
+        <Menu.Item
+          key={channel.id}
+          onClick={() => this.changeChannel(channel)}
+          name={channel.name}
+          style={{ opacity: 0.7 }}
+          active={channel.id === this.state.activechannel}
+        >
+          # {channel.name}
+        </Menu.Item>
+      ))
+    );
+  };
   render() {
+    const { starredChannels } = this.state;
     return (
       <Menu.Menu className="menu">
         <Menu.Item>
@@ -18,4 +50,4 @@ class Starred extends React.Component {
   }
 }
 
-export default Starred;
+export default connect(null, { setCurrentChannel, setPrivateChannel })(Starred);
